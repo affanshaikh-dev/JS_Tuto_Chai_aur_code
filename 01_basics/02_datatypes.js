23b7e6093255b7c20591fea475aacf727337489f
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-19 16:21:16 +0000

    datatype config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser
+let name = 'hitesh'
+let age = 18
+let isLoggedIn = false
+let state;
+let nullvalue = null;
+
+// number => Limit: 2 to power 53
+// bigint => for larger number
+// string => ""
+// boolean => true/false
+// null = standalone value
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof nullvalue);
