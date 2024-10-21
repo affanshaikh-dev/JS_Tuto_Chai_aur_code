156afa1bd52d5e16232795980905104089b8d6fe
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-21 04:05:19 +0000

    conversion config done

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,13 @@
+// let score = 93;
+// let score = "93";
+// let score = "93abc";
+// let score = null;
+// let score = undefined;
+let score = true;
+
+console.log(typeof score);
+
+let valueInNumeber = Number(score);
+console.log(typeof valueInNumeber);
+console.log(valueInNumeber); // NaN - not a number
+
