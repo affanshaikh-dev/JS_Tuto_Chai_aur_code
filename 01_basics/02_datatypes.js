d73b9328fce0fb26e9ea022d0637c6ade3fecd83
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-26 10:52:14 +0000

    converstion of datatype done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -17,8 +17,12 @@ let nullvalue = null;
 
 // object 
 
+/*
 console.log(typeof name);
 console.log(typeof age);
 console.log(typeof isLoggedIn);
 console.log(typeof state);
 console.log(typeof nullvalue);
+*/
+
+
--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -3,11 +3,28 @@
 // let score = "93abc";
 // let score = null;
 // let score = undefined;
-let score = true;
-
-console.log(typeof score);
+let score = false;
+// console.log(typeof score);
 
 let valueInNumeber = Number(score);
-console.log(typeof valueInNumeber);
-console.log(valueInNumeber); // NaN - not a number
+// console.log(typeof valueInNumeber);
+// console.log(valueInNumeber); // NaN - not a number
+
+// "33" => 33
+// "33abc" => NaN
+// true, false => 1, 0
+
+let isLoggedIn = 33;
+// let booleanConver = Boolean(isLoggedIn);
+// console.log(booleanConver);
+
+// 1 => true, 0 => false
+// "" => false
+// "string" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
