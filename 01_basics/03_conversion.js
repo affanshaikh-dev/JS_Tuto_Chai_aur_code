dd38873f9df77d194eb8d8735333f4a7656be382
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-26 11:15:31 +0000

    converstion done

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -25,6 +25,43 @@ let isLoggedIn = 33;
 let someNumber = 33;
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// Operation *******
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// Arithemetic Operators
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str = "Hello ";
+let str2 = "World";
+
+// console.log(str + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let n1, n2, n3;
+// n1 = n2 = n3 = 180 
+
+let gamecounter = 100;
+// console.log(gamecounter);
+// gamecounter++
+++gamecounter
+// console.log(gamecounter);
+
 
