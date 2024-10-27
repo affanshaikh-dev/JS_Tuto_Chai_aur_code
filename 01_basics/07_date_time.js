7dacb1dc425eebd6afaf9019b38df219576cf889
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-27 08:11:17 +0000

    date time done

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,39 @@
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+// let myCurrentDate = new Date(2023, 0, 23);
+// let myCurrentDate = new Date(2023, 0, 23, 5, 3);
+// let myCurrentDate = new Date("2023-01-14"); // yy-dd-mm
+let myCurrentDate = new Date("01-14-2023"); // mm-dd-yy
+// console.log(myCurrentDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCurrentDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date();
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// console.log(
+//     newDate.toLocaleString('default', {
+//         weekday: "long",
+//         day: "2-digit"
+//     })
+// )
+
+
+
+
+
