b90db5d3e64601094bf665abcc94bef1c6ecf9c2
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-27 07:27:55 +0000

    number math running

---
+++ 01_basics/06_number_math.js
@@ -0,0 +1,27 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherName = 23.8574
+// console.log(otherName.toPrecision(2));
+
+const salary = 1000000
+// console.log(salary.toLocaleString('en-IN'));
+
+// Maths +*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
+// console.log(Math.abs(-254));
+// console.log(Math.round(45.4));
+// console.log(Math.ceil(45.9));
+// console.log(Math.floor(45.9));
+// console.log(Math.min(4, 8, 4,2,1,0,8));
+// console.log(Math.max(4, 8, 4,2,1,0,8));
+
+console.log(Math.round(Math.random() * 6)); // range: 0 - 1
+
+
