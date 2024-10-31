44fe57f34c8c87be4c14ff823cf4db17ed7855ba
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-31 15:16:21 +0000

    filter done

---
+++ 05_itaration/06_filter.js
@@ -0,0 +1,67 @@
+// const coding = ['js', 'ruby', 'java', 'python', 'cpp']
+
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNum = myNums.filter((num) => {
+//     return num > 4;
+// })
+// console.log(newNum);
+
+// const newNums = []
+// myNums.forEach((num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+const books = [
+    {
+        title: 'Book One', 
+        genre: 'Fiction',
+        publish: '1981', 
+        edition: 2023 
+    },
+    {
+        title: 'Book Two', 
+        genre: 'Non-Fiction',
+        publish: '1999', 
+        edition: 2021 
+    },
+    {
+        title: 'Book Three', 
+        genre: 'History',
+        publish: '2000', 
+        edition: 2015 
+    },
+    {
+        title: 'Book Four', 
+        genre: 'Fiction',
+        publish: '1998', 
+        edition: 2003 
+    },
+    {
+        title: 'Book Five', 
+        genre: 'Fiction',
+        publish: '1988', 
+        edition: 2005 
+    },
+    {
+        title: 'Book Six', 
+        genre: 'History',
+        publish: '1999', 
+        edition: 2006 
+    }
+]
+
+// let userBook = books.filter((bk) => bk.genre === 'Fiction')
+// userBook = books.filter((bk) => {
+//     return bk.publish >= 1995 && bk.genre === 'History'
+// })
+// console.log(userBook);
