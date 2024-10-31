e900cd43e094cbf5beeb16af3d11885df2d6116d
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-31 16:06:25 +0000

    map reduce done

---
+++ 05_itaration/08_reduce.js
@@ -0,0 +1,31 @@
+const myNum = [1, 2, 3]
+
+// const myTotol = myNum.reduce((acc, curVal) => {
+//     console.log(`acc: ${acc} and curval: ${curVal}`);
+//     return acc + curVal;
+// }, 0)
+
+// const myTotol = myNum.reduce((acc, curVal) => acc + curVal, 5)
+// console.log(myTotol);
+
+const shoppingCart = [
+    {
+        itemName: "JS course", 
+        price: 2999
+    },
+    {
+        itemName: "CSS course", 
+        price: 2000
+    },
+    {
+        itemName: "HTML course", 
+        price: 1500
+    },
+    {
+        itemName: "Python course", 
+        price: 3000
+    }
+]
+
+// const totalPrice = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+// console.log(totalPrice);
