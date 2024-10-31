9245a34a37accac076a72ffe3a39b88721cde308
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-31 08:25:32 +0000

    first flow start

---
+++ 04_controlFlow/1_flow.js
@@ -0,0 +1,22 @@
+// Comparition operators
+// <
+// >
+// >=
+// <=
+// == 
+// ===
+// !=
+// !==
+
+// if statement
+// if(2 !== "2") {
+//     console.log("executed");   
+// }
+
+let temperature = 41
+
+if(temperature == 41) {
+    console.log(`Temperature is equal to ${temperature}`);
+} else {
+    console.log(`Temperature is not equal to ${temperature}`);
+}
