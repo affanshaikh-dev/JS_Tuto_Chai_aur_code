340d7525e8417324f17b1322b6c9c13ba6724195
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-10-31 11:55:10 +0000

    while loop done

---
+++ 05_itaration/02_while.js
@@ -0,0 +1,16 @@
+let i = 11;
+// while(i <= 10) {
+//     console.log(`Value of i is ${i}`);
+//     i++;
+// }
+
+// let arr = [1, 5, 2, 3 ,5 ,8]
+// while(i < arr.length){
+//     console.log(i);
+//     i++;
+// }
+
+do {
+    console.log(i);
+    i++;
+} while(i < 10);
