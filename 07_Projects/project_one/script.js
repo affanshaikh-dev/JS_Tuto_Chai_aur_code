a520e84a4f7ccda96d2dd0d41e01047dbff3ce5f
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-11-07 12:11:42 +0000

    little practice done

--- 07_Projects/project_one/script.js
+++ 07_Projects/project_one/script.js
@@ -1,25 +1,43 @@
 let btn = document.querySelectorAll(".btns")
 let body = document.querySelector("body")
 
+// method one
+// btn.forEach((btn) => {
+//     // console.log(btn);
+//     btn.addEventListener('click', (e) => {
+//         switch (e.target.id) {
+//             case 'green':
+//                 body.style.background = e.target.id
+//                 break;
+//             case 'red':
+//                 body.style.background = e.target.id
+//                 break;
+//             case 'blue':
+//                 body.style.background = e.target.id
+//                 break;
+//             case 'orange':
+//                 body.style.background = e.target.id
+//                 break;
+//             default:
+//                 alert("Error! Choose right Color!")
+//                 break;
+//         }
+//     })
+// }) 
+
+// method two
 btn.forEach((btn) => {
-    // console.log(btn);
     btn.addEventListener('click', (e) => {
-        switch (e.target.id) {
-            case 'green':
-                body.style.background = e.target.id
-                break;
-            case 'red':
-                body.style.background = e.target.id
-                break;
-            case 'blue':
-                body.style.background = e.target.id
-                break;
-            case 'orange':
-                body.style.background = e.target.id
-                break;
-            default:
-                alert("Error! Choose right Color!")
-                break;
+        if(e.target.id === "green") {
+            body.style.background = e.target.id;
+        } else if (e.target.id === "red") {
+            body.style.background = e.target.id;
+        } else if (e.target.id === "blue") {
+            body.style.background = e.target.id;
+        } else if (e.target.id === "orange") {
+            body.style.background = e.target.id;
+        } else {
+            alert("Error! Choose right Color.")
         }
     })
 }) 
