53f4f6bc1f9cd56140c9aed5d798ba922a4eaac9
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-11-07 15:28:59 +0000

    partial project four done

---
+++ 07_prac_project/project_four/script.js
@@ -0,0 +1,5 @@
+let ranNum = Math.floor(Math.random() * 100 + 1);
+let submit = document.querySelector("#sub")
+let guesses = document.querySelector(".guesses")
+let remaining = document.querySelector("lastResult")
+let LowOrHi = document.querySelector("LowOrHi")
