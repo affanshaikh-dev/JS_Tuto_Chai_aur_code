355cd415a2c38d6c81e46d8d351cefc2e698bf89
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-11-08 12:18:08 +0000

    on going

--- 07_prac_project/project_four/script.js
+++ 07_prac_project/project_four/script.js
@@ -1,5 +1,7 @@
 let ranNum = Math.floor(Math.random() * 100 + 1);
 let submit = document.querySelector("#sub")
 let guesses = document.querySelector(".guesses")
-let remaining = document.querySelector("lastResult")
-let LowOrHi = document.querySelector("LowOrHi")
+let remaining = document.querySelector(".lastResult")
+let LowOrHi = document.querySelector(".LowOrHi")
+let startOver = document.querySelector(".result")
+
