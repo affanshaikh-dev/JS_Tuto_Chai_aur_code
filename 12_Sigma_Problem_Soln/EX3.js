71769d1da491ef3754eb01cd9c0ee41a55a12932
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-03 13:38:52 +0000

    EX ongoing

---
+++ 12_Sigma_Problem_Soln/EX3.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+let num = parseInt(prompt("Enter Number to find factorial!"))
+let res;
+
+for(let i=1; i > num; i++) {
+    return res *= i 
+}
+
+console.log(res);
