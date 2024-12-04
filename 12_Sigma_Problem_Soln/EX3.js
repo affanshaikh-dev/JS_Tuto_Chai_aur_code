0f99e763d5e5ff9e2aebc33dc0360486a0cb86ae
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2024-12-04 08:24:38 +0000

    EX 3 done

--- 12_Sigma_Problem_Soln/EX3.js
+++ 12_Sigma_Problem_Soln/EX3.js
@@ -1,10 +1,25 @@
 const prompt = require('prompt-sync')();
 
-let num = parseInt(prompt("Enter Number to find factorial!"))
-let res;
+let num = parseInt(prompt("Enter Number to find factorial!"));
 
-for(let i=1; i > num; i++) {
-    return res *= i 
+// by itraition method
+// let res = 1;
+
+// for(let i = 1; i <= num; i++) {
+//     res *= i;
+// }
+
+// console.log(`The factorial of ${num} is ${res}`);
+
+// by reduce method
+function fac(number) {
+    let arr = Array.from(Array(number + 1).keys())
+    let c = arr.slice(1, ).reduce((a, b) => {
+        return a * b
+    })
+
+    console.log(c);
+    
 }
 
-console.log(res);
+fac(num)
